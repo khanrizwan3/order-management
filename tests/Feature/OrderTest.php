949e8b28e5cf0ba6c6da461bc8a00e2028d695dd
Rizwan Khan <rizwankhan@MUHAMMADs-MacBook-Pro.local>
2021-03-14 20:07:00 +0500

    adding test cases

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -7,51 +7,62 @@ use Illuminate\Foundation\Testing\WithFaker;
 use App\Http\Middleware\Authenticate;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Auth;
-use App\User;
+use App\Order;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 
 
 class OrderTest extends TestCase
 {
+    use WithoutMiddleware;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 
     public function test_order_index()
     {
         $response = $this->get('/orderAjax');
 
-        $response->assertStatus(302); // if run pass it means authorization redirection works.
+        $response->assertStatus(200); // if run pass it means order controller index action works.
     }
 
-    public function testLogin(){
+    public function testAddOrder(){
 
-        $user = factory(User::class)->create([
-            'name' => 'Test',
-            'email' => 'test@hotmail.com',
-            // note you need to use the bcrypt function here to hash your password
-            'password' => bcrypt('123456')
-        ]);
+        $response = $this->json('POST', '/orderAjaxCreate', ['user_id' => 1 , 'product_id' => 2, 'quantity' => 21 ]);
 
-        $response = $this->post('login', [
-            'name' => 'Test',
-            'email' => 'test@hotmail.com',
-            'password' => '123456'
-        ]);
-
-        //this works
-        $response->assertRedirect('/');
-
-        //this fails
-        $this->assertTrue(Auth::check());
+        $response
+            ->assertStatus(200)
+            ->assertExactJson([
+                Config('constants.api.success_status') => Config('constants.api.success_message'),
+            ]);
     }
+
+    public function testEditOrder(){
+
+        $response = $this->json('GET', '/orderAjaxEdit'.'/1');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath('id' , 1);
+    }
+
+    public function testOrderAjaxDelete(){
+
+        $response = $this->json('DELETE', '/orderAjaxDelete'.'/63');
+
+        $response
+            ->assertStatus(200)
+            ->assertExactJson([
+                Config('constants.api.success_status') => Config('constants.api.success_message'),
+            ]);
+    }
+
+
+
+
 }
